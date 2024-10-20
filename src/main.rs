666b37a09dfcca539cbc931104491682de512a5d
Yasuaki Gohko <ygohko@gmail.com>
2024-10-20 17:54:22 +0900

    Added TestSource type.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,53 @@ use std::time::Duration;
 use rodio::{Decoder, OutputStream, Sink};
 use rodio::source::{SineWave, Source};
 
+struct TestSource {
+    count: i32,
+}
+
+impl Source for TestSource {
+    fn current_frame_len(&self) -> Option<usize> {
+        Some(65536)
+    }
+
+    fn channels(&self) -> u16 {
+        1
+    }
+
+    fn sample_rate(&self) -> u32 {
+        44100
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
+
+impl Iterator for TestSource {
+    type Item = f32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let value: f32;
+        if ((self.count / 50) & 1) == 1 {
+            value = 1.0;
+        }
+        else {
+            value = 0.0;
+        }
+        self.count += 1;
+
+        Some(value)
+    }
+}
+
+impl TestSource {
+    fn new() -> Self {
+        return Self {
+            count: 0,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 
@@ -12,7 +59,8 @@ fn main() {
     let sink = Sink::try_new(&stream_handle).unwrap();
 
     // Add a dummy source of the sake of the example.
-    let source = SineWave::new(440.0).take_duration(Duration::from_secs_f32(0.25)).amplify(0.20);
+    // let source = SineWave::new(440.0).take_duration(Duration::from_secs_f32(3.0)).amplify(1.0);
+    let source = TestSource::new();
     sink.append(source);
 
     // The sound plays in a separate thread. This call will block the current thread until the sink
