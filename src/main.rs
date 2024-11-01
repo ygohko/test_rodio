97f024806c62c54939c568a49e3656e328ce63b6
Yasuaki Gohko <ygohko@gmail.com>
2024-11-01 19:44:49 +0900

    Clean up (#3)
    * Cleaned up.
    
    * Cleaned up.

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,13 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+use rodio::source::Source;
+use rodio::{OutputStream, Sink};
 use std::f32::consts;
 use std::fs::File;
 use std::path::Path;
-use std::time::Duration;
 use std::thread;
-use rodio::{OutputStream, Sink};
-use rodio::source::Source;
+use std::time::Duration;
 
 const SAMPLING_FREQUENCY: f32 = 24000.0;
 #[allow(dead_code)]
@@ -63,8 +63,7 @@ impl Iterator for TestSource {
         let value: f32;
         if ((self.count / 50) & 1) == 1 {
             value = 1.0;
-        }
-        else {
+        } else {
             value = 0.0;
         }
         self.count += 1;
@@ -76,9 +75,7 @@ impl Iterator for TestSource {
 impl TestSource {
     #[allow(dead_code)]
     fn new() -> Self {
-        return Self {
-            count: 0,
-        }
+        return Self { count: 0 };
     }
 }
 
@@ -130,8 +127,7 @@ impl WaveSource {
             let mut sample = angle.sin();
             if sample <= 0.0 {
                 sample = -1.0;
-            }
-            else {
+            } else {
                 sample = 1.0;
             }
             samples.push(sample);
@@ -157,7 +153,7 @@ impl WaveSource {
         let (header, samples) = wav_io::read_from_file(file).unwrap();
         println!("header: {:?}", header);
         for sample in samples {
-          result.samples.push(sample)  
+            result.samples.push(sample)
         }
 
         result
@@ -181,7 +177,7 @@ impl FtResult {
         }
     }
 
-    fn score(&self) ->f32 {
+    fn score(&self) -> f32 {
         let mut value = 0.0;
         for i in 0..PARAMETER_COUNT {
             value += self.a[i as usize].abs();
@@ -216,7 +212,8 @@ fn execute_ft(source: &WaveSource, base_frequency: f32, position: usize, count: 
             let sample = source.samples[j as usize];
             let value = angle.cos();
             sum += sample * value;
-            angle += 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * base_frequency * ((i + 1) as f32);
+            angle +=
+                2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * base_frequency * ((i + 1) as f32);
         }
         result.a.push(sum / (count as f32));
     }
@@ -230,7 +227,8 @@ fn execute_ft(source: &WaveSource, base_frequency: f32, position: usize, count: 
             let sample = source.samples[j as usize];
             let value = angle.sin();
             sum += sample * value;
-            angle += 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * base_frequency * ((i + 1) as f32);
+            angle +=
+                2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * base_frequency * ((i + 1) as f32);
         }
         result.b.push(sum / (count as f32));
     }
@@ -244,12 +242,22 @@ fn execute_ift(ft_result: &FtResult) -> WaveSource {
     for i in 0..SAMPLE_COUNT {
         let mut sample: f32 = ft_result.a0;
         for j in 0..PARAMETER_COUNT {
-            let angle = 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * ft_result.base_frequency * (i as f32) * ((j + 1) as f32);
+            let angle = 2.0
+                * consts::PI
+                * (1.0 / SAMPLING_FREQUENCY)
+                * ft_result.base_frequency
+                * (i as f32)
+                * ((j + 1) as f32);
             sample += ft_result.a[j as usize] * angle.cos();
         }
 
         for j in 0..PARAMETER_COUNT {
-            let angle = 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * ft_result.base_frequency * (i as f32) * ((j + 1) as f32);
+            let angle = 2.0
+                * consts::PI
+                * (1.0 / SAMPLING_FREQUENCY)
+                * ft_result.base_frequency
+                * (i as f32)
+                * ((j + 1) as f32);
             sample += ft_result.b[j as usize] * angle.cos();
         }
 
@@ -287,7 +295,11 @@ fn execute_dft(source: &WaveSource) -> Vec<FtResult> {
         }
 
         if result.a.len() != 0 && result.b.len() != 0 {
-            println!("base_frequency: {}, score: {}", result.base_frequency, result.score());
+            println!(
+                "base_frequency: {}, score: {}",
+                result.base_frequency,
+                result.score()
+            );
             results.push(result);
         }
         position += count;
@@ -302,12 +314,22 @@ fn execute_idft(results: &Vec<FtResult>, multiplier: f32) -> WaveSource {
         for i in 0..DFT_SAMPLE_COUNT {
             let mut sample: f32 = ft_result.a0;
             for j in 0..PARAMETER_COUNT {
-                let angle = 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * ft_result.base_frequency * (i as f32) * ((j + 1) as f32);
+                let angle = 2.0
+                    * consts::PI
+                    * (1.0 / SAMPLING_FREQUENCY)
+                    * ft_result.base_frequency
+                    * (i as f32)
+                    * ((j + 1) as f32);
                 sample += ft_result.a[j as usize] * angle.cos();
             }
 
             for j in 0..PARAMETER_COUNT {
-                let angle = 2.0 * consts::PI * (1.0 / SAMPLING_FREQUENCY) * ft_result.base_frequency * (i as f32) * ((j + 1) as f32);
+                let angle = 2.0
+                    * consts::PI
+                    * (1.0 / SAMPLING_FREQUENCY)
+                    * ft_result.base_frequency
+                    * (i as f32)
+                    * ((j + 1) as f32);
                 sample += ft_result.b[j as usize] * angle.cos();
             }
 
@@ -322,57 +344,17 @@ fn execute_idft(results: &Vec<FtResult>, multiplier: f32) -> WaveSource {
 }
 
 fn main() {
-    println!("Hello, world!");
-
-    // _stream must live as long as the sink
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
+
     let wave_source = WaveSource::load("assets/test.wav");
-    // let wave_source = WaveSource::new();
-
-    /*
-    let mut best_store: f32 = 0.0;
-    let mut result = FtResult::new();
-    for i in 220..441 {
-        let result1 = execute_ft(&wave_source, i as f32, 0, wave_source.samples.len());
-        let score = result1.score();
-        if score > best_store {
-            best_store = score;
-            result = result1;
-        }
-
-        println!("base_frequency: {}, score: {}", i, score);
-    }
-    println!("");
-
-    println!("base_frequency: {}, score: {}", result.base_frequency, result.score());
-    println!("a0: {}", result.a0);
-    for i in 0..result.a.len() {
-        println!("a{}: {}", i + 1, result.a[i]);
-    }
-    for i in 0..result.b.len() {
-        println!("b{}: {}", i + 1, result.b[i]);
-    }
-    println!("");
-
-    let wave_source1 = execute_ift(&result);
-    */
-
     let results = execute_dft(&wave_source);
-
     let wave_source2 = execute_idft(&results, 4.0);
-    
+
     sink.append(wave_source);
     sink.sleep_until_end();
 
     thread::sleep(Duration::from_secs(1));
-
-    /*
-    sink.append(wave_source1);
-    sink.sleep_until_end();
-
-    thread::sleep(Duration::from_secs(1));
-    */
 
     sink.append(wave_source2);
     sink.sleep_until_end();
